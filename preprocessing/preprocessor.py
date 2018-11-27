58011e3f05dfce7774b4c981d8780a3b25b58c1a
Justacode <Mustafinkzn97@gmail.ru>
2018-11-27 16:27:12 +0300

    Merge branch 'master' of C:\Users\Musta\PycharmProjects\sentiment-nn with conflicts.

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -10,10 +10,10 @@ import json
 
 class TextPreprocessor:
     # def __init__(self):
-    #     regexp_manager = RegexpManager()
-    #     self.regexps = regexp_manager.get_compiled()
+    #     self.regexps = RegexpManager().get_compiled()
 
     def preprocess(self, text):
+        text = self.clean_text(text)
         # summarized = self.summarize_contents(text)
         tokens = self.tokenize(text)
         tokens_without_stopwords = self.remove_stopwords(tokens)
@@ -40,12 +40,24 @@ class TextPreprocessor:
         stop_words = set(stopwords.words('english'))
         return [token for token in tokens if not token in stop_words]
 
+    def clean_text(self, text):
+        text = text.rstrip()
+
+        if '""' in text:
+            if text[0] == text[-1] == '"':
+                text = text[1:-1]
+            text = text.replace('\\""', '"')
+            text = text.replace('""', '"')
+
+        text = text.replace('\\""', '"')
+
+        text = html.unescape(text)
+        text = ' '.join(text.split())
+        return text
+
 
 # TEST_SET = "data/train/original/twitter-2013test-A.tsv"
 # test_data = pd.read_csv(TEST_SET, sep='\t',
 #                         header=None, names=["id", "sentiment", "text"],
 #                         usecols=["sentiment", "text"])
 
-# pre = TextPreprocessor()
-
-# ipdb.set_trace()
