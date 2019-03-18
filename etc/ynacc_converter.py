d7549b2bf060fe6ed2953481752ad017083050c8
Marsel Mustafin <mustafinkzn97@gmail.com>
2019-03-18 18:37:21 +0300

    Add filtering by length on ynacc corpus

--- etc/ynacc_converter.py
+++ etc/ynacc_converter.py
@@ -6,10 +6,11 @@ NAMES = ["sdid", "commentindex", "headline", "url", "guid", "commentid",
           "commentagreement", "topic", "intendedaudience", "persuasiveness"]
 SENTIMENTS = ["negative", "neutral", "positive"]
 
-YNACC_EXPERT_CORPUS = "data/train/ydata-ynacc-v1_0_expert_annotations.tsv"
+YNACC_EXPERT_CORPUS = "data/ydata-ynacc-v1_0_expert_annotations.tsv"
 CONVERTED_CORPUS = "data/train/ydata-ynacc-v1_0_expert_annotations_filt.tsv"
 
 data = pd.read_csv(YNACC_EXPERT_CORPUS, header=None, sep="\t", names=NAMES)
-no_mixed = data[data.sentiment.isin(SENTIMENTS)]
-no_mixed.to_csv(path_or_buf=CONVERTED_CORPUS, sep="\t", header=None,
+data = data[data.sentiment.isin(SENTIMENTS)]
+data = data[data.apply(lambda x: len(x.text.split(" ")) < 90, axis=1)]
+data.to_csv(path_or_buf=CONVERTED_CORPUS, sep="\t", header=None,
                 index=False, columns=["commentid", "sentiment", "text"])
